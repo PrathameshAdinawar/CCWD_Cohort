9be5d676eb07fa85732567100f23a4fa72d37e62
Prathamesh Adinawar <prathamesh.adi1@gmail.com>
2026-05-12 23:30:13 +0530

    React, ReactDOM, @Vitejs/plugin-react, JSX

---
+++ React_Sratch/07/src/App.jsx
@@ -0,0 +1,7 @@
+export function App() {
+    return (
+        <div>
+            <h1>Hello Ji</h1>
+        </div>
+    )
+}
---
+++ React_Sratch/07/src/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from 'react-dom/client' // provides functionality of VDOM
+import { App } from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
