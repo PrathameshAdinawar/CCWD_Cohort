4a61a940d2bfc68f0d371e92d0c672e06e47a31f
Prathamesh Adinawar <prathamesh.adi1@gmail.com>
2026-05-14 02:13:33 +0530

    Build the Nodejs to understand the working 🙃

---
+++ React_Sratch/10/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function App() {
+    return React.createElement('div', { style: { color: 'orange' } },
+        React.createElement('h1', null, "Hello World!")
+    )
+}
