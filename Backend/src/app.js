ffb3260025024acc66b8c2a4af4aea41537fca4f
Prathamesh Adinawar <prathamesh.adi1@gmail.com>
2026-08-31 20:26:54 +0530

    Disk-storage + random file name  + extension

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,33 +1,44 @@
 import express from "express"
 import cookieParser from 'cookie-parser'
 import authRoute from './module/auth/auth.route.js'
+
 import multer from 'multer'
 import Apireponse from "./common/utils/api-repsonse.js"
 import ApiError from "./common/utils/api-error.js"
+import path from "path"
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
+// Multer: middleware to handle multipart/form-data (file uploads), since Express can't parse it natively
+//Storage config
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads')
+    },
+
+    // This part of filename is just avoid the same file name conflict
+    // else 1st users data can get override by another one with same name
+    filename: function (req, file, cb) {
+
+        // without the extension the file we are uploading will be useless
+        // so we extract the extension using the dependency "path"
+        const ext = path.extname(file.originalname)
+
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix + ext)
+    }
+})
 
 
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, 'public/uploads')
-//     },
-//     filename: function (req, file, cb) {
-//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-//         cb(null, file.fieldname + '-' + uniqueSuffix)
-//     }
-// })
-
-// const storage = multer.memorystorage();
+//Disk storage
+const upload = multer({ storage });
 
 
-
-const upload = multer();
-
+// In-memory storage
+// const upload = multer();
 
 app.post("/upload", upload.single("file"), (req, res) => {
     console.log(req.file);
